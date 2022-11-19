80fd3c03ea87aae9da00cfc0346c30bd487cdd28
Jeffrey Omondi <11554jeff@gmail.com>
2022-11-19 21:26:14 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-// Update to have your correct username and password
+// Update to have your correct username and password here
 config.mongoURI = {
   // production:
   //   "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
