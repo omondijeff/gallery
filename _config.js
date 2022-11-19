616064c6003ff9cd53228fb713b45a800a00b68d
Jeffrey Omondi <11554jeff@gmail.com>
2022-11-19 10:36:06 +0300

    MomgoDb. Setup

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production:
+    "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+  development:
+    "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+  test: "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+};
 module.exports = config;
