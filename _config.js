fe0a8e83f3e2b37c73ed9374566883b99e06fd02
Jeffrey Omondi <11554jeff@gmail.com>
2022-11-19 21:22:43 +0300

    MongoDb Atlas

--- _config.js
+++ _config.js
@@ -2,10 +2,16 @@ var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
+  // production:
+  //   "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+  // development:
+  //   "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+  // test: "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+
   production:
-    "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://admin:zidVtnf082Vxx3Ry@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
   development:
-    "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
-  test: "mongodb+srv://admin:#Moring2022@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://admin:zidVtnf082Vxx3Ry@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
+  test: "mongodb+srv://admin:zidVtnf082Vxx3Ry@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority",
 };
 module.exports = config;
